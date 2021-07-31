3715d7003f36d426348fc17ecc71134c58e712cc
JeUpark <genhcode99@gmail.com>
2021-07-31 11:21:20 +0900

    #3.2 Injecting The Repository

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { Restaurant } from "./entities/restaurant.entity"
 import { RestaurantsResolver } from "./restaurants.resolver"
+import { RestaurantService } from "./restaurants.service"
 
 @Module({
-  providers: [RestaurantsResolver],
+  // [TypeOrmModule.forFeature([Restaurant])] -> Repository 를 import 함.
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+  providers: [RestaurantsResolver, RestaurantService],
 })
 export class RestaurantsModule {}
