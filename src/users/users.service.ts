73da5ee49bb9137680aebd2cf23a601fee468db8
JeUpark <genhcode99@gmail.com>
2021-08-01 17:45:47 +0900

    #4.6 An Alternative Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,15 +15,16 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exist = await this.userDB.findOne({ email })
       if (exist) {
-        return "There is a user with that email already"
+        return { ok: false, error: "There is a user with that email already" }
       }
       await this.userDB.save(this.userDB.create({ email, password, role }))
+      return { ok: false, error: null }
     } catch (e) {
-      return "Couldn't create account."
+      return { ok: false, error: "Couldn't create account." }
     }
   }
 }
