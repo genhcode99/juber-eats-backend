a6d7eb257cb92c29171d12145af705afbc063c2c
JeUpark <genhcode99@gmail.com>
2021-08-02 13:00:37 +0900

    #5.4 JWT Module part Two

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { JwtModule } from "./jwt/jwt.module"
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_DATABASE: Joi.string().required(),
-        SECRET_KEY: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
       }),
     }),
 
@@ -39,7 +39,9 @@ import { JwtModule } from "./jwt/jwt.module"
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    JwtModule.forRoot(),
+    JwtModule.forRoot({
+      privateKey: process.env.PRIVATE_KEY,
+    }),
     UsersModule,
     CommonModule,
   ],
--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from "@nestjs/common"
+import { CONFIG_OPTIONS } from "./jwt.constants"
+import { JwtModuleOptions } from "./jwt.interfaces"
+import { Inject, Injectable } from "@nestjs/common"
 
 @Injectable()
 export class JwtService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {}
+
   hello() {
     console.log("hello")
   }
