1bb161c1484a256a0cc8c1484bfe8078a6ef48d7
JeUpark <genhcode99@gmail.com>
2021-08-03 15:11:40 +0900

    #6.0 Verification Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { UsersModule } from "./users/users.module"
 import { User } from "./users/entities/user.entity"
 import { JwtMiddleware } from "./jwt/jwt.middleware"
+import { Verification } from "./users/entities/verification.entity"
 
 @Module({
   imports: [
@@ -39,7 +40,7 @@ import { JwtMiddleware } from "./jwt/jwt.middleware"
       database: process.env.DB_DATABASE,
       synchronize: process.env.NODE_ENV !== "prod",
       logging: process.env.NODE_ENV !== "prod",
-      entities: [User],
+      entities: [User, Verification],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
