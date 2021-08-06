bc193a6573a5cc6eb083ce24221a8377c751af27
JeUpark <genhcode99@gmail.com>
2021-08-07 07:31:18 +0900

    #9.3 Testing createAccount part Two

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,12 @@ import { AppModule } from "../src/app.module"
 import { INestApplication } from "@nestjs/common"
 import { Test, TestingModule } from "@nestjs/testing"
 
+jest.mock("got", () => {
+  return {
+    post: jest.fn(),
+  }
+})
+
 const GRAPHQL_ENDPOINT = "/graphql"
 
 describe("UserModule (e2e)", () => {
@@ -27,6 +33,7 @@ describe("UserModule (e2e)", () => {
   describe("createAccount", () => {
     const EMAIL = "nico@las.com"
 
+    // 1. 유저 계정이 만들어졌을 경우
     it("should create account", () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
@@ -49,6 +56,34 @@ describe("UserModule (e2e)", () => {
         .expect((res) => {
           expect(res.body.data.createAccount.ok).toBeTruthy()
           expect(res.body.data.createAccount.error).toBe(null)
+        })
+    })
+
+    // 2. 이미 email 이 존재하는 경우
+    it("should fail if account already exists", () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            createAccount(
+              input:{
+                email:"${EMAIL}",
+                password:"test.password",
+                role:Client
+              }){
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBeFalsy()
+          expect(res.body.data.createAccount.error).toEqual(
+            "There is a user with that email already",
+          )
         })
     })
   })
