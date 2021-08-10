b3bb2838853b2a2ac4a76479a1b81d3cea5de21e
JeUpark <genhcode99@gmail.com>
2021-08-10 17:13:15 +0900

    #11.9 Create Order part Four

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,10 +3,12 @@ import { Order } from "./entities/order.entity"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { OrdersService } from "./orders.service"
 import { OrdersResolver } from "./orders.resolver"
+import { OrderItem } from "./entities/order-item.entity"
 import { Restaurant } from "src/restaurants/entities/restaurant.entity"
+import { Dish } from "src/restaurants/entities/dish.entity"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Restaurant])],
+  imports: [TypeOrmModule.forFeature([Order, Restaurant, OrderItem, Dish])],
   providers: [OrdersResolver, OrdersService],
 })
 export class OrdersModule {}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
+import { Dish } from "src/restaurants/entities/dish.entity"
 import { Restaurant } from "src/restaurants/entities/restaurant.entity"
 import { User } from "src/users/entities/user.entity"
 import { Repository } from "typeorm"
 import { CreateOrderInput, CreateOrderOutput } from "./dtos/create-order.dto"
+import { OrderItem } from "./entities/order-item.entity"
 import { Order } from "./entities/order.entity"
 
 @Injectable()
@@ -12,8 +14,14 @@ export class OrdersService {
     @InjectRepository(Order)
     private readonly ordersDB: Repository<Order>,
 
+    @InjectRepository(OrderItem)
+    private readonly orderItemsDB: Repository<OrderItem>,
+
     @InjectRepository(Restaurant)
     private readonly restaurantsDB: Repository<Restaurant>,
+
+    @InjectRepository(Dish)
+    private readonly dishesDB: Repository<Dish>,
   ) {}
 
   // Create Order
@@ -31,14 +39,24 @@ export class OrdersService {
         }
       }
 
-      const order = await this.ordersDB.save(
-        this.ordersDB.create({
-          customer,
-          restaurant,
-        }),
-      )
-
-      console.log(order)
+      items.forEach(async (item) => {
+        const dish = await this.dishesDB.findOne(item.dishId)
+        if (!dish) {
+          // abort this whole thing.
+        }
+        await this.orderItemsDB.save(
+          this.orderItemsDB.create({
+            dish,
+            options: item.options,
+          }),
+        )
+      })
+      // const order = await this.ordersDB.save(
+      //   this.ordersDB.create({
+      //     customer,
+      //     restaurant,
+      //   }),
+      // )
     } catch (e) {
       return {
         ok: false,
