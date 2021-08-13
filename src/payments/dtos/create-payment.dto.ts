ea3fa23caf5c4004e467664e2bf2dc6f6c4c93a5
JeUpark <genhcode99@gmail.com>
2021-08-13 16:09:57 +0900

    #13.2 createPayment part One

---
+++ src/payments/dtos/create-payment.dto.ts
@@ -0,0 +1,8 @@
+import { Field, InputType, Int, PickType } from "@nestjs/graphql"
+import { Payment } from "../entities/payment.entity"
+
+@InputType()
+export class CreatePaymentInput extends PickType(Payment, [
+  "transactionId",
+  "restaurantId",
+]) {}
---
+++ src/payments/payments.service.ts
@@ -0,0 +1,12 @@
+import { Inject, Injectable } from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository } from "typeorm"
+import { Payment } from "./entities/payment.entity"
+
+@Injectable()
+export class PaymentsService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentsDB: Repository<Payment>,
+  ) {}
+}
