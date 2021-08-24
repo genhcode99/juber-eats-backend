8c245f63d1e426244c2f93f43f0f325bbfa63608
genhcode99 <genhcode99@gmail.com>
2021-08-24 19:28:32 +0900

    #20.3 File Upload part Two

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,11 +1,44 @@
+import {
+  Controller,
+  Injectable,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common"
+import * as AWS from "aws-sdk"
+import { ConfigService } from "@nestjs/config"
 import { FileInterceptor } from "@nestjs/platform-express"
-import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common"
 
+const BUCKET_NAME = "juber-eats-s3-upload"
+
+@Injectable()
 @Controller("uploads")
 export class UploadsController {
+  constructor(private readonly config: ConfigService) {}
+
   @Post()
   @UseInterceptors(FileInterceptor("file"))
-  uploadFile(@UploadedFile() file) {
-    console.log(file)
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      region: "ap-northeast-2",
+      credentials: {
+        accessKeyId: this.config.get("S3_ACCESS_KEY"),
+        secretAccessKey: this.config.get("S3_SECRET_ACCESS_KEY"),
+      },
+    })
+    try {
+      const objectName = `${Date.now() + file.originalname}`
+      const { Location: url } = await new AWS.S3()
+        .upload({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          Key: objectName,
+          ACL: "public-read",
+        })
+        .promise()
+      return { url }
+    } catch (e) {
+      return null
+    }
   }
 }
--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
 import { UploadsController } from "./uploads.controller"
 
 @Module({
+  imports: [ConfigService],
   controllers: [UploadsController],
 })
 export class UploadsModule {}
