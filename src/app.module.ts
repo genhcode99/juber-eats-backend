1b30041ac8871f8a95201adb87ee23aa5cc7ef36
genhcode99 <genhcode99@gmail.com>
2021-08-28 21:31:00 +0900

    fix: playground 사용 제한 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,9 +27,9 @@ import { UploadsModule } from "./uploads/uploads.module"
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
-      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      ignoreEnvFile: process.env.NODE_ENV === "production",
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid("dev", "prod", "test"),
+        NODE_ENV: Joi.string().valid("dev", "production", "test"),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -67,6 +67,7 @@ import { UploadsModule } from "./uploads/uploads.module"
     }),
 
     GraphQLModule.forRoot({
+      playground: process.env.NODE_ENV !== "production",
       installSubscriptionHandlers: true,
       autoSchemaFile: true,
       // http 링크에는 req가 있고 websoket에는 connection 이 있다.
